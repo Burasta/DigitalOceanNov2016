71accf1b2463aa0e248cf8c1681aed764bc1c9a9
Brandon Camp <Brandon-Camp@GeorgiaSouthern.edu>
2015-10-21 08:54:47 -0400

    yay!

--- twittenby.py
+++ twittenby.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
+import sqlalchemy
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
@@ -12,6 +12,9 @@ def hello_world():
 def hello2():
     return render_template('test.html')
 
+@app.route('/tweets')
+def tweets():
+    return render_template('tweets.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
